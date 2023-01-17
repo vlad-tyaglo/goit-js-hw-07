138b66c4f2daaea558febc4d8d688fc1bae626f1
Vladislav Tyaglo <vlad.tyaglo95@gmail.com>
2023-01-17 23:23:32 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map((item) =>
+        `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a>`)
+    .join("");
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', { close: false, captionDelay: 250, captionsData: 'alt' });
